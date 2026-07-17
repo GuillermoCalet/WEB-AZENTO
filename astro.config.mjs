f9ed43a0bf2166a598f0b2b52b26002f13c4ce59
GuillermoCalet <guillermo.calet@gmail.com>
2026-07-18 01:49:35 +0200

    configurar adapter de vercel para tina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import tina from '@tinacms/astro/integration';
 import { tinaAdminDevRedirect } from '@tinacms/astro/vite';
 
+const isVercel = Boolean(process.env.VERCEL);
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
+  adapter: isVercel ? vercel() : node({ mode: 'standalone' }),
   integrations: [tailwind(), react(), tina()],
   vite: {
     plugins: [tinaAdminDevRedirect()],
